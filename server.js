dc4397065eac0873a063168730dd26f9f8cabf51
VoidChaos <VoidChaos@github.com>
2017-08-14 20:00:03 +1000

    improvements

--- server.js
+++ server.js
@@ -4,7 +4,26 @@ var fs = require('fs');
 var url = require('url');
 var bodyParser = require('body-parser');
 var app = express();
-
+var crypto = require('crypto'),
+algorithm = 'aes-256-ctr',
+password = 'd6F3Efeq';
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+ 
+var hw = encrypt("hello world")
+// outputs hello world
+console.log(decrypt(hw));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
